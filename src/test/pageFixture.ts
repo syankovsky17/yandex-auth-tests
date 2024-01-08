0cd0535a9ad299079cea5c9bb99536e30b15ab70
Sergey Yankovskiy <s.yankovsky@a1qa.com>
2024-01-08 13:52:57 +0300

    FIX: File duplication

--- src/test/pageFixture.ts
+++
@@ -1,9 +0,0 @@
-import { Page } from '@playwright/test';
-
-export const pageFixture = {
-  page: undefined as unknown as Page,
-
-  initializePage: async function (browserContext: any) {
-    this.page = await browserContext.newPage();
-  },
-};
