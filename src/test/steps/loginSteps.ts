67d5a61c99ba42589f91acb3fd4ab47ca2bd81ef
Sergey Yankovskiy <s.yankovsky@a1qa.com>
2024-01-08 17:03:40 +0300

    FIX: Type definition removed

--- src/test/steps/loginSteps.ts
+++ src/test/steps/loginSteps.ts
@@ -2,7 +2,6 @@ import { Given, When, Then, setDefaultTimeout } from '@cucumber/cucumber';
 import { expect } from '@playwright/test';
 import { pageFixture } from '../../hooks/pageFixture';
 
-
 setDefaultTimeout(60 * 1000 * 2);
 const baseURL = 'https://passport.yandex.ru/auth';
 
@@ -10,7 +9,7 @@ Given('I am on the Yandex login page', async function () {
     await pageFixture.page.goto(baseURL);
 });
 
-When('I fill in the email field with {string}', async function (email) {
+When('I fill in the email field with {string}', async (email) => {
   await pageFixture.page.waitForSelector('#passp-field-login');
   await pageFixture.page.fill('#passp-field-login', email);
 });
@@ -28,18 +27,18 @@ When('I click the {string} button', async (buttonName) => {
   await pageFixture.page.getByRole('button', { name: buttonName }).click();
 });
 
-When('I fill in the password field with {string}', async (password: string) => {
+When('I fill in the password field with {string}', async (password) => {
   await pageFixture.page.getByPlaceholder('Введите пароль').fill(password);
 });
 
-Then('I see the error message {string}', async (expectedErrorMessage: string) => {
+Then('I see the error message {string}', async (expectedErrorMessage) => {
     const errorElement = await pageFixture.page.locator(`//*[@id[starts-with(., 'field:input')]]`);
     const actualText = await errorElement.textContent();    
     const regex = new RegExp(expectedErrorMessage.replace(/ /g, '\\s*'));
     expect(actualText).toMatch(regex);
 });
 
-Then('I see the message {string}',async (expectedMessage:string) => {
+Then('I see the message {string}',async (expectedMessage) => {
     const messageLocator = await pageFixture.page.waitForSelector(`//*[contains(text(), "${expectedMessage}")]`)
     const isVisible = !!await messageLocator.isVisible();
     expect(isVisible).toBe(true);
